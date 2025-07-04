17c036505561241ed61a37090e63e15ca1443c74
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2025-07-05 02:29:47 +0300

    Simple working config

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
+# ~/nixos-config/flake.nix
 {
   description = "My NixOS Configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    agenix.url = "github:ryantm/agenix";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./hardware-configuration.nix
-        ];
-      };
+  outputs = { self, nixpkgs, agenix, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+        agenix.nixosModules.default
+      ];
     };
   };
 }
